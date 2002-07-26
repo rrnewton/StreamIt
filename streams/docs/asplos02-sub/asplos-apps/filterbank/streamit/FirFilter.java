b72825b00ebb55ad45da68f727e970d5ba4375c3
saadat <saadat@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2002-07-26 09:43:45 +0000

    This completes the implementation of a single pipeline in a filter bank.
    git-svn-id: https://svn.csail.mit.edu/streamit/trunk@2071 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/docs/asplos02-sub/asplos-apps/filterbank/streamit/FirFilter.java
+++ streams/docs/asplos02-sub/asplos-apps/filterbank/streamit/FirFilter.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2001 Massachusetts Institute of Technology
+ *  Copyright 2002 Massachusetts Institute of Technology
  *
  *  Permission to use, copy, modify, distribute, and sell this software and its
  *  documentation for any purpose is hereby granted without fee, provided that
@@ -11,6 +11,9 @@
  *  suitability of this software for any purpose.  It is provided "as is"
  *  without express or implied warranty.
  */
+
+
+// Together with a delay this creats an FIR 
 
 import streamit.*;
 
