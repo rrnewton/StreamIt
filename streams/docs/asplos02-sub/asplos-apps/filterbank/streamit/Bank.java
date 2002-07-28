3ac69168739d3c6c13bd5301a57c42b741e6cce7
saadat <saadat@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2002-07-28 22:55:50 +0000

    This is the Complete Filter bank. Some debugging underway to the java library.
    git-svn-id: https://svn.csail.mit.edu/streamit/trunk@2131 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/docs/asplos02-sub/asplos-apps/filterbank/streamit/Bank.java
+++ streams/docs/asplos02-sub/asplos-apps/filterbank/streamit/Bank.java
@@ -24,75 +24,23 @@ import streamit.io.*;
  * Implements an FIR Filter
  */
 
-public class Bank extends StreamIt {
+public class Bank extends Pipeline {
  
 
-    
-class source extends Filter {
-    int N;
-    float[] r;
-    public source(float[] r) {super(r);}
-    public void init(float[] r){
-	output = new Channel(Float.TYPE,r.length);
-	this.r=r;
-	N=r.length;
-    }
-    public void work(){
-	for(int i=0;i<N;i++)
-	     output.pushFloat(r[i]);
-    }
-}
-
-class sink extends Filter{
-    int N;
-    public sink(int N) {super(N);}
-    public void init(int N){
-	input = new Channel(Float.TYPE, N);
-	this.N=N;
-	//setPop(N);
-    }
-    public void work() {
-	System.out.println("Starting");
-
-	for (int i=0; i< N;i++)
-	    {
-		System.out.print("This is ");
-		System.out.print(i);
-		System.out.print(" : ");
-		System.out.println(input.popFloat());
-	    }
-	    
-    }
-}
-
-
-        
-    static public void main(String[] t)
+    public Bank (int N,float[] H,float[] F)
     {
-	Bank test=new Bank();
-	test.run(t);
-    }
+        super (N,H,F);
+	}
 
-
-    /*   public Bank (int N,float[] H,float[] F)
-    {
-        //super (N,H,F);
-	}*/
-
-    public void init( /* int N,float[] H,float[] F */) {
+    public void init(  int N,float[] H,float[] F ) {
 	
-	  float[] H={1,2,3};
-          float[] F={2,1,3};
-	  float[] r={1,2,3,4,1,2};
-	  int N=2;
-
   
-	add (new source(r));
+	//add (new source(r)); They are here for debugging purposes
 	add (new FIR(H));
 	add (new DownSamp(N));
 	add (new UpSamp(N));
 	add (new FIR(F));
-	add (new sink(r.length));
+	//add (new sink(r.length));
     }
     
 
