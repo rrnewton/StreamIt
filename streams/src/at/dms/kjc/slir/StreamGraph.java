d77c77be7fd0068a7d460aa96372e8f07b312ac9
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2011-08-04 19:29:17 +0000

    Fixed assertion no size of SSGs.
    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11287 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/src/at/dms/kjc/slir/StreamGraph.java
+++ streams/src/at/dms/kjc/slir/StreamGraph.java
@@ -16,72 +16,64 @@ import at.dms.kjc.common.CommonUtils;
 /**
  * 
  * @author mgordon
- *
+ * 
  */
 public class StreamGraph {
-    
- 
+
 	protected int steadyMult;
 
-    // sirfilter -> work estimation
-    // TODO: DELETE 
-    protected WorkEstimate work;
+	// sirfilter -> work estimation
+	// TODO: DELETE
+	protected WorkEstimate work;
 
- 
-   
-    // TODO: We need to fix the hierarchy. StreamGraph is being used
+	// TODO: We need to fix the hierarchy. StreamGraph is being used
 	// more like an ssg, but it should represent multiple ssgs.
 	List<StaticSubGraph> ssgs;
 
-
-
 	/**
-     * Create a Partitioner.
-     * 
-     * The number of partitions may be limited by <i>maxPartitions</i>, but
-     * some implementations ignore <i>maxPartitions</i>.
-     * 
-     * @param topFilters  from {@link FlattenGraph}
-     * @param exeCounts  a schedule
-     * @param lfa  a linearAnalyzer to convert filters to linear form if appropriate.
-     * @param work a work estimate, see {@link at.dms.kjc.sir.lowering.partition}, updeted if filters are added to a slice.
-     * @param maxPartitions if non-zero, a maximum number of partitions to create
-     */
-    // DELETE work abd exeCounts
-    public StreamGraph() {    
-        steadyMult = KjcOptions.steadymult;
-    
-    }
-
-
+	 * Create a Partitioner.
+	 * 
+	 * The number of partitions may be limited by <i>maxPartitions</i>, but some
+	 * implementations ignore <i>maxPartitions</i>.
+	 * 
+	 * @param topFilters
+	 *            from {@link FlattenGraph}
+	 * @param exeCounts
+	 *            a schedule
+	 * @param lfa
+	 *            a linearAnalyzer to convert filters to linear form if
+	 *            appropriate.
+	 * @param work
+	 *            a work estimate, see {@link at.dms.kjc.sir.lowering.partition}
+	 *            , updeted if filters are added to a slice.
+	 * @param maxPartitions
+	 *            if non-zero, a maximum number of partitions to create
+	 */
+	// DELETE work abd exeCounts
+	public StreamGraph() {
+		steadyMult = KjcOptions.steadymult;
+		ssgs = new ArrayList<StaticSubGraph>();
+	}
 
 	public StaticSubGraph getSSG(int i) {
-    	return ssgs.get(i);
-    }
-
+		return ssgs.get(i);
+	}
 
 	public StaticSubGraph getSSG() {
-		assert ssgs.size() == 0 : "Calling getSSG() on a graph with dynamic rates, and thus multiple SSGs!";
-    	return ssgs.get(0);
-    }
+		assert ssgs.size() == 1 : "Calling getSSG() on a graph with dynamic rates, and thus multiple SSGs!";
+		return ssgs.get(0);
+	}
 
- 
-    
-    public int getSteadyMult() {
+	public int getSteadyMult() {
 		return steadyMult;
 	}
 
-
-    
-    public void setSteadyMult(int steadyMult) {
+	public void setSteadyMult(int steadyMult) {
 		this.steadyMult = steadyMult;
 	}
 
-    public int getNumSSGs() {
-    	return ssgs.size();
-    }
-    
+	public int getNumSSGs() {
+		return ssgs.size();
+	}
+
 }
-    
-
-
