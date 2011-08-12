c44de8d1ec514b29c4303cead5b18e06f8183452
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2011-08-12 17:50:58 +0000

    Renamed variables to make more sense.
    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11310 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/src/at/dms/kjc/sir/lowering/SegmentedSIRGraph.java
+++ streams/src/at/dms/kjc/sir/lowering/SegmentedSIRGraph.java
@@ -35,20 +35,20 @@ import at.dms.kjc.sir.StreamVisitor;
  */
 public class SegmentedSIRGraph implements StreamVisitor {
 
-	/** This is the list of all pipelines created */
-	private List<SIRStream> staticSubGraphs = null;
-
 	/**
 	 * Maps source to sinks to indicate connections between static subgraphs.
 	 * Divergent operators will have multiple connections.
 	 */
 	private Map<SIRStream, List<SIRStream>> connections = null;
 
-	/** These are the child nodes of the pipeline currently being created */
-	private LinkedList<Object> pipelines = null;
-
 	/** An identifier to distinguish pipeline names */
 	private int pipelineId = 0;
+
+	/** These are the child nodes of the pipeline currently being created */
+	private LinkedList<Object> pipelineMembers = null;
+
+	/** This is the list of all pipelines created */
+	private List<SIRStream> staticSubGraphs = null;
 
 	/** a flag to turn on debugging */
 	boolean debug = true;
@@ -61,13 +61,48 @@ public class SegmentedSIRGraph implements StreamVisitor {
 		connections = new HashMap<SIRStream, List<SIRStream>>();
 	}
 
+	/**
+	 * Add a new static subsection to the graph. Each subsection must be a
+	 * pipeline with a source, some number of other filters, and a sink.
+	 * 
+	 * @param pipeline
+	 *            subsection to add
+	 * @return void
+	 */
+	public void addToSegmentedGraph(SIRStream pipeline) {
+		staticSubGraphs.add(pipeline);
+	}
+
+	/**
+	 * Returns the connections between static subsections.
+	 * 
+	 * @return the connections between static subsections.
+	 */
+	public Map<SIRStream, List<SIRStream>> getConnections() {
+		return connections;
+	}
+
+	public SIRStream getStaticSubGraph0() {
+		assert staticSubGraphs.size() == 1 : "SegmentedGraph.getStaticSubGraph0 staticSubGraph should have only one subsection in static case.";
+		return staticSubGraphs.get(0);
+	}
+
+	/**
+	 * Returns the list of static subsections
+	 * 
+	 * @return the list of static subsections
+	 */
+	public List<SIRStream> getStaticSubGraphs() {
+		return staticSubGraphs;
+	}
+
 	public SegmentedSIRGraph init(SIRStream str, boolean isDynamic) {		
 		if (!isDynamic) {
-			this.addPipe(str);
+			this.addToSegmentedGraph(str);
 			return this;
 		} 
 
-		pipelines = new LinkedList<Object>();
+		pipelineMembers = new LinkedList<Object>();
 
 		IterFactory.createFactory().createIter(str).accept(this);
 
@@ -91,156 +126,6 @@ public class SegmentedSIRGraph implements StreamVisitor {
 		log("*********************************");
 
 		return this;
-	}
-
-	/**
-	 * Add a new static subsection to the graph. Each subsection must be a
-	 * pipeline with a source, some number of other filters, and a sink.
-	 * 
-	 * @param pipeline
-	 *            subsection to add
-	 * @return void
-	 */
-	public void addPipe(SIRStream pipeline) {
-		staticSubGraphs.add(pipeline);
-	}
-
-	/**
-	 * Returns the connections between static subsections.
-	 * 
-	 * @return the connections between static subsections.
-	 */
-	public Map<SIRStream, List<SIRStream>> getConnections() {
-		return connections;
-	}
-
-	/**
-	 * Returns the list of static subsections
-	 * 
-	 * @return the list of static subsections
-	 */
-	public List<SIRStream> getStaticSubGraphs() {
-		return staticSubGraphs;
-	}
-
-	public SIRStream getStaticSubGraph0() {
-		assert staticSubGraphs.size() == 1 : "SegmentedGraph.getStaticSubGraph0 staticSubGraph should have only one subsection in static case.";
-		return staticSubGraphs.get(0);
-	}
-
-	private void log(String str) {
-		if (debug)
-			System.out.println(str);
-	}
-
-	private SIRFilter createSink(SIRFilter pred, SIRFilterIter iter) {
-		SIRFilter sink = new SIRDummySink(pred.getOutputType());
-		log(this.getClass().getCanonicalName() + " createSink()");
-		return sink;
-	}
-
-	private SIRFilter createSource(SIRFilter succ, SIRFilterIter iter) {
-		SIRFilter source = new SIRDummySource(succ.getInputType());
-		log(this.getClass().getCanonicalName() + " createSource()");
-		return source;
-	}
-
-	private void finishPipeline(SIRFilter self, SIRFilterIter iter) {
-		/* add the source and sink */
-		if (!isSink(self)) {
-			log(this.getClass().getCanonicalName() + " adding a sink after "
-					+ self.getName());
-			pipelines.add(createSink(self, iter));
-		}
-		if (!isSource(self)) {
-			log(this.getClass().getCanonicalName() + " adding a source before "
-					+ self.getName());
-			pipelines.add(0, createSource((SIRFilter) pipelines.get(0), iter));
-		}
-		SIRPipeline pipeline = new SIRPipeline(null, uniquePipelineName());
-		pipeline.setInit(SIRStream.makeEmptyInit());
-		pipeline.setChildren(pipelines);
-		addPipe(pipeline);
-		pipelines = new LinkedList<Object>();
-		log(this.getClass().getCanonicalName() + " pipeline.size()="
-				+ pipeline.size());
-	}
-
-	private boolean isDynamicPop(SIRFilter self) {
-		return self.getPop().isDynamic();
-	}
-
-	private boolean isDynamicPush(SIRFilter self) {
-		return self.getPush().isDynamic();
-	}
-
-	private boolean isSink(SIRFilter self) {
-		return (self.getOutputType() == CStdType.Void);
-	}
-
-	private boolean isSource(SIRFilter self) {
-		return (self.getInputType() == CStdType.Void);
-	}
-
-	private String uniquePipelineName() {
-		pipelineId++;
-		return "SSGPipeline" + pipelineId;
-	}
-
-	@Override
-	public void visitFilter(SIRFilter self, SIRFilterIter iter) {
-		log(this.getClass().getCanonicalName() + " visitFilter()");
-		log(this.getClass().getCanonicalName() + " self.getPopString="
-				+ self.getPopString());
-		log(this.getClass().getCanonicalName() + " self.getPushString="
-				+ self.getPushString());
-		log(this.getClass().getCanonicalName() + " self.getPeekString="
-				+ self.getPeekString());
-		log(this.getClass().getCanonicalName() + " isDynamicPush="
-				+ isDynamicPush(self));
-		log(this.getClass().getCanonicalName() + " isDynamicPop="
-				+ isDynamicPop(self));
-		log(this.getClass().getCanonicalName() + " iter.getPos="
-				+ iter.getPos());
-
-		SIRFilter filter = (SIRFilter) ObjectDeepCloner.deepCopy(self);
-
-		// If its a dynamic push, then add this filter, and create
-		// a new pipeline
-		if (isDynamicPush(filter)) {
-			filter.setPush(new JIntLiteral(1));
-			if (isSource(filter)) {
-				filter.setPop(new JIntLiteral(1));
-				filter.setPeek(new JIntLiteral(1));
-			}
-			pipelines.add(filter);
-			finishPipeline(filter, iter);
-			return;
-		}
-
-		// If its a dynamic pop, check to see if there is anything
-		// preceeding the filter that should be in its own pipeline
-		if (isDynamicPop(filter)) {
-			if (pipelines.size() > 0) {
-				finishPipeline(filter, iter);
-				return;
-			}
-			filter.setPop(new JIntLiteral(1));
-			filter.setPeek(new JIntLiteral(1));
-			pipelines.add(filter);
-			if (isSink(filter)) {
-				finishPipeline(filter, iter);
-				return;
-			}
-		}
-	}
-
-	/******************************* Code below is not used for now ******************************/
-
-	@Override
-	public void visitPhasedFilter(SIRPhasedFilter self, SIRPhasedFilterIter iter) {
-		throw new java.lang.RuntimeException(
-				"Dynamic rates do not support SIRPhasedFilters.");
 	}
 
 	@Override
@@ -282,6 +167,121 @@ public class SegmentedSIRGraph implements StreamVisitor {
 		{ /* Do nothing yet */
 		}
 		// if (self.getSplitter().getType().isDynamic()) { }
+	}
+
+	@Override
+	public void visitFilter(SIRFilter self, SIRFilterIter iter) {
+		log(this.getClass().getCanonicalName() + " visitFilter()");
+		log(this.getClass().getCanonicalName() + " self.getPopString="
+				+ self.getPopString());
+		log(this.getClass().getCanonicalName() + " self.getPushString="
+				+ self.getPushString());
+		log(this.getClass().getCanonicalName() + " self.getPeekString="
+				+ self.getPeekString());
+		log(this.getClass().getCanonicalName() + " isDynamicPush="
+				+ isDynamicPush(self));
+		log(this.getClass().getCanonicalName() + " isDynamicPop="
+				+ isDynamicPop(self));
+		log(this.getClass().getCanonicalName() + " iter.getPos="
+				+ iter.getPos());
+
+		SIRFilter filter = (SIRFilter) ObjectDeepCloner.deepCopy(self);
+
+		// If its a dynamic push, then add this filter, and create
+		// a new pipeline
+		if (isDynamicPush(filter)) {
+			filter.setPush(new JIntLiteral(1));
+			if (isSource(filter)) {
+				filter.setPop(new JIntLiteral(1));
+				filter.setPeek(new JIntLiteral(1));
+			}
+			pipelineMembers.add(filter);
+			finishPipeline(filter, iter);
+			return;
+		}
+
+		// If its a dynamic pop, check to see if there is anything
+		// preceeding the filter that should be in its own pipeline
+		if (isDynamicPop(filter)) {
+			if (pipelineMembers.size() > 0) {
+				finishPipeline(filter, iter);
+				return;
+			}
+			filter.setPop(new JIntLiteral(1));
+			filter.setPeek(new JIntLiteral(1));
+			pipelineMembers.add(filter);
+			if (isSink(filter)) {
+				finishPipeline(filter, iter);
+				return;
+			}
+		}
+	}
+
+
+
+	@Override
+	public void visitPhasedFilter(SIRPhasedFilter self, SIRPhasedFilterIter iter) {
+		throw new java.lang.RuntimeException(
+				"Dynamic rates do not support SIRPhasedFilters.");
+	}
+
+	private SIRFilter createSink(SIRFilter pred, SIRFilterIter iter) {
+		SIRFilter sink = new SIRDummySink(pred.getOutputType());
+		log(this.getClass().getCanonicalName() + " createSink()");
+		return sink;
+	}
+
+	private SIRFilter createSource(SIRFilter succ, SIRFilterIter iter) {
+		SIRFilter source = new SIRDummySource(succ.getInputType());
+		log(this.getClass().getCanonicalName() + " createSource()");
+		return source;
+	}
+
+	private void finishPipeline(SIRFilter self, SIRFilterIter iter) {
+		/* add the source and sink */
+		if (!isSink(self)) {
+			log(this.getClass().getCanonicalName() + " adding a sink after "
+					+ self.getName());
+			pipelineMembers.add(createSink(self, iter));
+		}
+		if (!isSource(self)) {
+			log(this.getClass().getCanonicalName() + " adding a source before "
+					+ self.getName());
+			pipelineMembers.add(0, createSource((SIRFilter) pipelineMembers.get(0), iter));
+		}
+		SIRPipeline pipeline = new SIRPipeline(null, uniquePipelineName());
+		pipeline.setInit(SIRStream.makeEmptyInit());
+		pipeline.setChildren(pipelineMembers);
+		addToSegmentedGraph(pipeline);
+		pipelineMembers = new LinkedList<Object>();
+		log(this.getClass().getCanonicalName() + " pipeline.size()="
+				+ pipeline.size());
+	}
+
+	private boolean isDynamicPop(SIRFilter self) {
+		return self.getPop().isDynamic();
+	}
+
+	private boolean isDynamicPush(SIRFilter self) {
+		return self.getPush().isDynamic();
+	}
+
+	private boolean isSink(SIRFilter self) {
+		return (self.getOutputType() == CStdType.Void);
+	}
+
+	private boolean isSource(SIRFilter self) {
+		return (self.getInputType() == CStdType.Void);
+	}
+
+	private void log(String str) {
+		if (debug)
+			System.out.println(str);
+	}
+
+	private String uniquePipelineName() {
+		pipelineId++;
+		return "SSGPipeline" + pipelineId;
 	}
 
 }
