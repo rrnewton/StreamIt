84dd3d0dd8acf5997558d65127fafeb84ac07794
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2011-10-31 23:52:56 +0000

    fixed adding of dominated filters
    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11431 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/src/at/dms/kjc/smp/ThreadMapper.java
+++ streams/src/at/dms/kjc/smp/ThreadMapper.java
@@ -33,15 +33,15 @@ public class ThreadMapper {
 			Map<String, String> dominators,
 			Map<Integer, String> threadIdToType
 			) {
-						
-		
+
+
 		boolean isDynamicInput = ssg.hasDynamicInput();
 		Filter f = ssg.getTopFilters()[0];
-		
+
 		boolean isStateful = f.getWorkNodeContent().isStateful();
-		
+
 		System.out.println("ThreadMapper.assignThreads filter = " + f.getWorkNode().toString() + " isStateful=" + isStateful);
-		
+
 		/* Check if it has a dynamic pop rate */
 		if (isDynamicInput || isStateful) {
 			Filter[] topFilters = ssg.getTopFilters();
@@ -51,24 +51,24 @@ public class ThreadMapper {
 				System.out.println("ThreadMapper.assignThreads Filter "
 						+ dynamicReader.getWorkNodeContent().getName()
 						+ " is a dynamic reader");
-				
+
 				//List<InterSSGEdge> links = ssg.getInputPort().getLinks();
-//				if (ssg.getInputPort() != null) {
-//				List<InterSSGEdge> links = ssg.getInputPort().getLinks();
-//				
-//					for (InterSSGEdge edge : links) {
-//						System.out.println("ThreadMapper.assignThreads edge = "
-//								+ edge.toString());
-//						Filter[] connectedGraph = edge.getDest().getSSG()
-//								.getFilterGraph();
-//						Filter connected = connectedGraph[connectedGraph.length - 1];
-//						System.out
-//								.println("ThreadMapper.assignThreads edge filter = "
-//										+ connected.getWorkNode().toString());
-//						filterToThreadId.put(connected, threadId);
-//					}				
-//				}
-				
+				//				if (ssg.getInputPort() != null) {
+				//				List<InterSSGEdge> links = ssg.getInputPort().getLinks();
+				//				
+				//					for (InterSSGEdge edge : links) {
+				//						System.out.println("ThreadMapper.assignThreads edge = "
+				//								+ edge.toString());
+				//						Filter[] connectedGraph = edge.getDest().getSSG()
+				//								.getFilterGraph();
+				//						Filter connected = connectedGraph[connectedGraph.length - 1];
+				//						System.out
+				//								.println("ThreadMapper.assignThreads edge filter = "
+				//										+ connected.getWorkNode().toString());
+				//						filterToThreadId.put(connected, threadId);
+				//					}				
+				//				}
+
 				CType type = dynamicReader.getWorkNodeContent().getOutputType();
 				//CType type = dynamicReader.getWorkNodeContent().getInputType();
 
@@ -76,13 +76,11 @@ public class ThreadMapper {
 						+ " has threadId=" + threadId
 						+ " and type=" + type.toString()
 						);
-				
-				filterToThreadId.put(dynamicReader, threadId);
-				
-				
-				if (type != CStdType.Void && type != null) {
 
-					
+				filterToThreadId.put(dynamicReader, threadId);
+
+
+				if (type != CStdType.Void && type != null) {					
 					threadIdToType.put(threadId, type.toString());												
 					Filter[] filterGraph = ssg.getFilterGraph();				
 					for (Filter filter : filterGraph) {
@@ -93,10 +91,12 @@ public class ThreadMapper {
 								+ " dominates "
 								+ filter.getWorkNodeContent().getName()
 								);
-						dominated.add(filter.getWorkNodeContent().getName());
+						if (isDynamicInput) {
+							dominated.add(filter.getWorkNodeContent().getName());
+						}
 						dominators.put(dynamicReader.getWorkNodeContent().getName(), filter.getWorkNodeContent().getName());
 					}
-				
+
 				}
 				threadId++;
 			}
