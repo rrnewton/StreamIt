50465aa975dc0db3c077212cff2aad1e61e9a9fc
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2011-12-07 01:50:03 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11465 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/qmtest/run.sh
+++ streams/apps/tests/dynamic/qmtest/run.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# remove old tests
+
+if [ -d ${STREAMIT_HOME}/users.qms ]
+then
+rm -rf ${STREAMIT_HOME}/users.qms
+fi
+
 #clean up old test
 ${STREAMIT_HOME}/regtest/qmtest/streamitqm clean
 
 # create the xml file
 echo "<regtest>
-  <test root=\"${STREAMIT_HOME}/apps/tests\"/>
+  <test root=\"${STREAMIT_HOME}/apps/tests/complex\"/>
+  <test root=\"${STREAMIT_HOME}/apps/tests/fuse\"/>
   <option target=\"smp2\"/>
 </regtest> " > smp.xml
 
