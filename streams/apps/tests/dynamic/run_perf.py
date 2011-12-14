ae13ade6142a855053c7eaa7601c7c09f7e0e4eb
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2011-12-14 03:13:17 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11509 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/run_perf.py
+++ streams/apps/tests/dynamic/run_perf.py
@@ -17,9 +17,9 @@ def print_header():
 def write_header(f):
     f.write('#%s\t%s\t%s\n' % ('cost', 'static', 'dynamic'))
 
-def print_result(cost, sta_avg, dyn_avg):    
+def print_result(f, cost, sta_avg, dyn_avg):    
     print('%d\t%f\t%f' % (cost, sta_avg, dyn_avg))
-    #f.write('%d\t%f\t%f' % (cost, sta_avg, dyn_avg))
+    f.write('%d\t%f\t%f\n' % (cost, sta_avg, dyn_avg))
 
 def compile(num_cores, num_iters, n, test):
     target = os.path.join(test_cases, test)
@@ -53,11 +53,25 @@ def replace(test, cost):
     o.write( re.sub("COST_VALUE",str(cost),data)  )
     o.close()
 
+def plot():
+    f = open('./tmp.gnu', 'w')
+    cmd = "plot \"results.dat\" u 1:2 t \'static\' w linespoints, \"results.dat\" u 1:3 t \'dynamic\' w linespoints"
+    f.write('set terminal postscript\n')
+    f.write('set output \"results.ps\"\n')
+    #f.write('set title \"Static vs Dynamic, Iterations=1000, Cost=%d,\"\n' % cost)
+    f.write('set xlabel \"Cost\"\n');
+    f.write('set ylabel \"Clock Cycles\"\n');
+    f.write(cmd)
+    f.close()
+    os.system('gnuplot ./tmp.gnu')
+
 def main():
     cores = [2]
     iterations = [10]
     nvalues = [1]
-    costs = [1, 10, 100, 1000]
+    costs = [1, 10, 100, 1000]    
+    f = open('./results.dat', 'w')
+    write_header(f)
     print_header()
     for cost in costs:
         for num_cores in cores:
@@ -70,8 +84,9 @@ def main():
                 static_avg = get_result(num_cores, static_test)
                 #print 'dynamic_avg time is %f' % dynamic_avg
                 #print 'static_avg time is %f' % static_avg
-                print_result(cost, static_avg, dynamic_avg)    
-                   
+                print_result(f, cost, static_avg, dynamic_avg)
+    f.close()
+    plot()
 
 
 
