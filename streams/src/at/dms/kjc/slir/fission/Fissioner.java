319958ac929dd80362ac420b3ae73063e37b66a8
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2011-12-14 15:44:51 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11510 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/src/at/dms/kjc/slir/fission/Fissioner.java
+++ streams/src/at/dms/kjc/slir/fission/Fissioner.java
@@ -106,6 +106,7 @@ public class Fissioner {
             return false;
         }
         
+        
         //TODO: make sure the rates match between the slice and its inputs and the slices 
         //and its outputs
 
