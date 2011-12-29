90f85415f357bef7c2e982ea9604938cd9aa0e54
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2011-12-29 00:53:57 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11539 fb122d4e-f3b4-4596-99d2-990b948fa53d

---
+++ streams/src/at/dms/kjc/sir/SIRWriter.java
@@ -0,0 +1,58 @@
+package at.dms.kjc.sir;
+
+import at.dms.kjc.CStdType;
+import at.dms.kjc.JExpression;
+import at.dms.kjc.JFieldDeclaration;
+import at.dms.kjc.JIntLiteral;
+import at.dms.kjc.JMethodDeclaration;
+import at.dms.kjc.JStringLiteral;
+import at.dms.kjc.sir.lowering.Propagator;
+
+/**
+ * A StreaMIT filter that writes a file to a data source. It is more
+ * general than the SIRFileWriter in that in can also write to stdout
+ * or stderr.
+ */
+public class SIRWriter extends SIRPredefinedFilter implements Cloneable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The filename of the data source.
+	 */
+	private JExpression fileName;
+
+	public SIRWriter() {
+		super(null,
+				"Writer",
+				/* fields */ JFieldDeclaration.EMPTY(),
+				/* methods */ JMethodDeclaration.EMPTY(),
+				new JIntLiteral(null, 1),
+				new JIntLiteral(null, 1),
+				new JIntLiteral(null, 0),
+				null, CStdType.Void);
+		this.fileName = new JStringLiteral("");
+	}
+
+	public void setFileName(JExpression fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFileName() {
+		if (!(fileName instanceof JStringLiteral)) {
+			System.err.println("Error:  have not yet resolved filename for filereader.\n" +
+					"        the filename expression is " + fileName);
+			new RuntimeException().printStackTrace();
+			System.exit(1);
+		}
+		return ((JStringLiteral)fileName).stringValue();
+	}
+
+	public void propagatePredefinedFields(Propagator propagator) {
+		JExpression newFilename = (JExpression)fileName.accept(propagator);
+		if (newFilename!=null && newFilename!=fileName) {
+			fileName = newFilename;
+		}
+	}
+
+}
