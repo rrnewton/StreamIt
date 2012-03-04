815d007d60068f9270472bb2dbb06ecc03c97e2a
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2012-03-04 19:11:47 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11709 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/operators/inputs/generate.py
+++ streams/apps/tests/dynamic/operators/inputs/generate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import struct
+import random
 
     
 def main():
@@ -9,7 +10,10 @@ def main():
         for i in range(1, 1000):
             s = struct.pack('f', 1.0)
             f.write(s)
-
+            r = random.uniform(1, 10)
+            print r
+            x = random.randint(1, 10) 
+            print x
                     
 if __name__ == "__main__":
     main()
