282b50e204d45c96ef91670224a45025993f0663
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2012-03-05 17:43:39 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11712 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/operators/inputs/generate.py
+++ streams/apps/tests/dynamic/operators/inputs/generate.py
@@ -7,13 +7,16 @@ import random
 def main():
     print 'hello'
     with open('tmp.bin', 'wb') as f:        
-        for i in range(1, 1000):
-            s = struct.pack('f', 1.0)
-            f.write(s)
-            r = random.uniform(1, 10)
-            print r
-            x = random.randint(1, 10) 
-            print x
+        for i in range(1, 100):
+            #r = random.uniform(1, 10)            
+            x = random.randint(1, 10)
+            for j in range(1, x):
+                print str(i) +  ',' +  str(x)
+                time = struct.pack('f', i)
+                val = struct.pack('f', x)
+                f.write(time)
+                f.write(val)
+
                     
 if __name__ == "__main__":
     main()
