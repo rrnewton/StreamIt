6021a937ec8393ebc3e3f4e579ed6e8a0ea0508d
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2012-03-05 18:46:42 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11715 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/operators/huffman/mkinputs.py
+++ streams/apps/tests/dynamic/operators/huffman/mkinputs.py
@@ -5,17 +5,26 @@ import random
 
     
 def main():
-    print 'hello'
+    input_text =  "this is an example of a huffman tree"
     with open('tmp.bin', 'wb') as f:        
-        for i in range(1, 100):
-            #r = random.uniform(1, 10)            
-            x = random.randint(1, 10)
-            for j in range(1, x):
-                print str(i) +  ',' +  str(x)
-                time = struct.pack('f', i)
-                val = struct.pack('f', x)
-                f.write(time)
-                f.write(val)
+        for c in input_text:
+            print c
+            x = ord(c)
+            print x
+            val = struct.pack('i', x)
+            f.write(val)
+
+    # print 'hello'
+    # with open('tmp.bin', 'wb') as f:        
+    #     for i in range(1, 100):
+    #         #r = random.uniform(1, 10)            
+    #         x = random.randint(1, 10)
+    #         for j in range(1, x):
+    #             print str(i) +  ',' +  str(x)
+    #             time = struct.pack('f', i)
+    #             val = struct.pack('f', x)
+    #             f.write(time)
+    #             f.write(val)
 
                     
 if __name__ == "__main__":
