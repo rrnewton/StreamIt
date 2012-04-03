e0f7073a4922e468a5aa7d5f366eaf612d563ab8
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2012-04-03 13:25:04 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11934 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/synthetic/des/run_batch.py
+++ streams/apps/tests/dynamic/synthetic/des/run_batch.py
@@ -14,9 +14,9 @@ streamit_home = os.environ['STREAMIT_HOME']
 strc          = os.path.join(streamit_home, 'strc')
 
 def compile(cores, test, work, ignore):
-    cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(work), '--preoutputs', str(ignore), --threadbatch', '512', '--noiter', 'DES2.str']    
+    cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(work), '--preoutputs', str(ignore), '--threadbatch', '512', '--noiter', 'DES2.str']    
     if test == Configs.dynamic:
-        cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(work), '--preoutputs', str(ignore), "--threadopt", '--noiter', '--threadbatch', '512', 'DES2Dynamic.str']    
+        cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(work), '--preoutputs', str(ignore), '--threadopt', '--noiter', '--threadbatch', '512', 'DES2Dynamic.str']    
     print ' '.join(cmd)
     subprocess.call(cmd, stdout=FNULL, stderr=FNULL)
     exe = './smp' + str(cores)     
