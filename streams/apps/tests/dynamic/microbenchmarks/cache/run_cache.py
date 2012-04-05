509d468aaeffaffbfb7833bbdb7361a744ebce75
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2012-04-05 16:45:56 +0000

    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@11948 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/microbenchmarks/cache/run_cache.py
+++ streams/apps/tests/dynamic/microbenchmarks/cache/run_cache.py
@@ -94,7 +94,7 @@ def run(test, attempts):
     # 1000000000 nanoseconds in 1 second    
     times = map(lambda x:  (long(x[4]) * 1000000000L) + long(x[5]) , results)
     tputs =  map(lambda x: 134217726/x * 1000000000L , times)
-    mean = reduce(lambda x, y: float(x) + float(y), times) / len(tputs)    
+    mean = reduce(lambda x, y: float(x) + float(y), tputs) / len(tputs)    
     deviations = map(lambda x: x - mean, tputs)
     squares = map(lambda x: x * x, deviations)
     dev = math.sqrt(reduce(lambda x, y: x + y, squares) /  (len(squares) - 1))
