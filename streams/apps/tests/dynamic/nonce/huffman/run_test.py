87ee07d445dc26fa6496c629b297951652f4bf43
(no author) <(no author)@fb122d4e-f3b4-4596-99d2-990b948fa53d>
2012-07-19 23:22:45 +0000

    Added Huffman Experiment
    git-svn-id: https://svn.csail.mit.edu/streamit/branches/streamit-3.0@12039 fb122d4e-f3b4-4596-99d2-990b948fa53d

--- streams/apps/tests/dynamic/nonce/huffman/run_test.py
+++ streams/apps/tests/dynamic/nonce/huffman/run_test.py
@@ -14,24 +14,22 @@ class Configs:
 streamit_home = os.environ['STREAMIT_HOME']
 strc          = os.path.join(streamit_home, 'strc')
 
-def generate(selectivity, outputs):
-    print 'generate ' + str(selectivity)
-    with open('floats.in', 'wb') as f:                
+def generate(outputs):
+    print 'generate' 
+    with open('ints.in', 'wb') as f:
+        # this is an example of a huffman tree
         for j in range(outputs):
-            val = struct.pack('f', 2.0)  # time
-            f.write(val)                 # val
-            val = struct.pack('f', 1.0)
-            f.write(val)
-            for i in range(1, selectivity):
-                val = struct.pack('f', 1.0) # time
-                f.write(val)       
-                val = struct.pack('f', 1.0) # val
-                f.write(val)       
-
+            for i in [116, 104, 105, 115, 32, 105, 115, 32, 97, 110,
+                      32, 101, 120, 97, 109, 112, 108, 101, 32, 111,
+                      102, 32, 97, 32, 104, 117, 102, 102, 109, 97,
+                      110, 32, 116, 114, 101, 101 ] :
+                val = struct.pack('i', i) 
+                f.write(val) 
+    
 def compile(cores, test, outputs, ignore):
-    cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(outputs), '--preoutputs', str(ignore), '--noiter', '--selective', 'VWAPStatic.str']    
+    cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(outputs), '--preoutputs', str(ignore), '--noiter', '--selective', 'HuffmanStatic.str']    
     if test == Configs.dynamic:
-        cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(outputs), '--preoutputs', str(ignore), "--threadopt", '--noiter', '--selective', 'VWAPDynamic.str']    
+        cmd = ["strc", "-smp", str(cores), "--perftest", "--outputs", str(outputs), '--preoutputs', str(ignore), "--threadopt", '--noiter', '--selective', 'HuffmanDynamic.str']    
     print ' '.join(cmd)
     subprocess.call(cmd, stdout=FNULL, stderr=FNULL)
     exe = './smp' + str(cores)     
@@ -69,95 +67,81 @@ def run(test, cores, attempts, outputs):
     return (mean, dev)
 
 def print_all(static_results, dynamic_results):
-    file = 'vwap.dat'
+    file = 'huffman.dat'
     with open(file, 'w') as f:
-        s = '#selectivity\t'
-        s += '\t'.join(["%s-sta-avg\t%s-sta-dev" % (s[2], s[2]) for s in static_results[0]])
-        s += '\t' + '\t'.join(["%s-dyn-avg\t%s-dyn-dev" % (s[2], s[2]) for s in dynamic_results[0]])
+        s = '#selectivity\tsta-avg\tsta-dev\tdyn-avg\tdyn-dev'
         print s
         f.write(s + '\n')    
         for static, dynamic in zip(static_results, dynamic_results):
-            s = '%d\t' % (static[0][1]) 
-            s += '\t'.join(["%f\t%f" % (d[3], d[4]) for d in static])
-            s += '\t' + '\t'.join(["%f\t%f" % (d[3], d[4]) for d in dynamic])
+            s = '%d\t%f\t%f' % (static[1], static[2], static[3])
+            s += '\t%f\t%f' % (dynamic[2], dynamic[3]) 
             print s
-            f.write(s + '\n')      
-    file = 'vwap-normalized.dat'
-    with open(file, 'w') as f:
-        s = '#selectivity\t'
-        s += '\t'.join(["%s-sta-norm\t%s-sta-dev" % (x[2], x[2]) for x in static_results[0]])
-        s += '\t' + '\t'.join(["%s-dyn-avg\t%s-dyn-avg" % (x[2], x[2]) for x in dynamic_results[0]])
-        print s
-        f.write(s + '\n')    
-        for static, dynamic in zip(static_results, dynamic_results):
-            #base =  static[0][3]
-            s = '%d\t' % (static[0][1]) 
-            s += '\t'.join(["%f\t%f" % (d[3], d[4]) for d in static])
-            s += '\t' + '\t'.join(["%f\t%f" % (d[3], d[4]) for d in dynamic])
-            print s
-            f.write(s + '\n')    
+            f.write(s + '\n')
+            
+    # file = 'vwap-normalized.dat'
+    # with open(file, 'w') as f:
+    #     s = '#selectivity\t'
+    #     s += '\t'.join(["%s-sta-norm\t%s-sta-dev" % (x[2], x[2]) for x in static_results[0]])
+    #     s += '\t' + '\t'.join(["%s-dyn-avg\t%s-dyn-avg" % (x[2], x[2]) for x in dynamic_results[0]])
+    #     print s
+    #     f.write(s + '\n')    
+    #     for static, dynamic in zip(static_results, dynamic_results):
+    #         #base =  static[0][3]
+    #         s = '%d\t' % (static[0][1]) 
+    #         s += '\t'.join(["%f\t%f" % (d[3], d[4]) for d in static])
+    #         s += '\t' + '\t'.join(["%f\t%f" % (d[3], d[4]) for d in dynamic])
+    #         print s
+    #         f.write(s + '\n')    
          
 
-def plot_normalized(cores):
-    data = 'vwap-normalized.dat'
-    output = 'vwap-normalized.ps'  
+def plot(cores):
+    data = 'huffman.dat'
+    output = 'huffman.ps'  
     cmd = "plot "
-    i = 2    
-    for core in cores:
-        if i != 2:
-            cmd += ', '
-        cmd += "\"" + data + "\" u 1:" + str(i) + " t \'static-" + str(core) + " \' w linespoints lc rgb \"blue\""
-        cmd += ", \"" + data + "\" u 1:" + str(i) + ":" + str(i+1) + " notitle w yerrorbars"
-        i = i + 2
-    for core in cores:
-        cmd += ", \"" + data + "\" u 1:" + str(i) + " t \'dynamic-" + str(core) + "\' w linespoints lc rgb \"red\""
-        cmd += ", \"" + data + "\" u 1:" + str(i) + ":" + str(i+1) + " notitle w yerrorbars"
-        i = i + 2
-    with open('./vwap-normalized.gnu', 'w') as f:        
+    cmd += "\"" + data + "\" u 1:2 t \'static\' w linespoints lc rgb \"blue\""
+    cmd += ", \"" + data + "\" u 1:2:3 notitle w yerrorbars"
+    cmd += ", \"" + data + "\" u 1:4 t \'dynamic\' w linespoints lc rgb \"red\""
+    cmd += ", \"" + data + "\" u 1:4:5 notitle w yerrorbars"  
+    with open('./huffman.gnu', 'w') as f:        
         f.write('set terminal postscript\n')
         f.write('set output \"' + output + '\"\n')
         f.write('set key left center\n');
-        f.write('set logscale x\n');
-        f.write('set title \"VWAP Operator Normalized\"\n')
-        f.write('set xlabel \"Frequency\"\n');
-        f.write('set ylabel \"Throughput normalized to static throughput with 1 core\"\n');
+        #f.write('set logscale x\n');
+        f.write('set title \"Huffman Encoder / Decoder\"\n')
+        f.write('set xlabel \"Cores\"\n');
+        f.write('set ylabel \"Throughput\"\n');
         f.write(cmd)
-    os.system('gnuplot ./vwap-normalized.gnu')
+    os.system('gnuplot ./huffman.gnu')
     os.system('ps2pdf ' + output)
 
 def main():    
     attempts = 3
     ignore = 32
     outputs = 10000
-    selectivities = [1, 10, 100, 1000, 10000]
-    cores = [1,8]
+    cores = [1,2,4,8,16]
     static_results = []
     dynamic_results = []
     with open('./partial-results.dat', 'w') as f:        
-        for selectivity in selectivities:
-            static = []
-            dynamic = []
-            for core in cores:        
-                for test in [Configs.static, Configs.dynamic]:
-                    generate(selectivity, outputs)
-                    compile(core, test, outputs, ignore)
-                    (avg, dev) =  run(test, core, attempts, outputs)
-                    if test == Configs.static:
-                        x = ('static', selectivity, core, avg, dev)
-                        s = 'static\t%d\t%d\t%f\t%f' % (selectivity, core, avg, dev)
-                        f.write(s + '\n')   
-                        print x
-                        static.append(x)
-                    elif test == Configs.dynamic:
-                        x = ('dynamic', selectivity, core, avg, dev)
-                        s = 'dynamic\t%d\t%d\t%f\t%f' % (selectivity, core, avg, dev)
-                        f.write(s + '\n')   
-                        print x          
-                        dynamic.append(x)
-            dynamic_results.append(dynamic)
-            static_results.append(static)
+        generate(outputs)
+        for core in cores:        
+            for test in [Configs.static, Configs.dynamic]:    
+                compile(core, test, outputs, ignore)
+                (avg, dev) =  run(test, core, attempts, outputs)
+                if test == Configs.static:
+                    x = ('static', core, avg, dev)
+                    s = 'static\t%d\t%f\t%f' % (core, avg, dev)
+                    f.write(s + '\n')   
+                    print x
+                    static_results.append(x)
+                elif test == Configs.dynamic:
+                    x = ('dynamic', core, avg, dev)
+                    s = 'dynamic\t%d\t%f\t%f' % (core, avg, dev)
+                    f.write(s + '\n')   
+                    print x          
+                    dynamic_results.append(x)
+
         print_all(static_results, dynamic_results)
-        plot_normalized(cores)
+        plot(cores)
 
                     
 if __name__ == "__main__":
